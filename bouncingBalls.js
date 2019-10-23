7dae94fd136568d73e1f6652f509f454d50c1c38
Vasil Todorovski <vasko16@gmail.com>
2019-10-23 15:22:32 +0200

    Fixed precision of getSpeed* functions Fixed condition for inactive ball removal Added random color to each ball on creation

--- bouncingBalls.js
+++ bouncingBalls.js
@@ -4,9 +4,9 @@ const CANVAS_ID = "playground";
 
 const MAX_SPEED = 100;	// max speed in pixels per second
 const FRAME_RATE = 25;	// render executions per second
-const BALL_RADIUS = 5; 	// ball radius in pixels
+const BALL_RADIUS = 10; // ball radius in pixels
 const GRAVITY = 9.8;	// pixels per second^2
-const BALL_MASS = 0.5; 	// kg
+const BALL_MASS = 5; 	// kg
 const DRAG_COEF = 0.01;
 const ELASTIC_COEF = 0.9;	// must be between 0 and 1
 
@@ -18,7 +18,8 @@ function throwBall(x, y) {
 		x: x,
 		y: y,
 		speed: Math.random() * MAX_SPEED,
-		direction: Math.random() * Math.PI * 2
+		direction: Math.random() * Math.PI * 2,
+		color: "#" + Math.random().toString(16).slice(-6)
 		// speed: 1 * MAX_SPEED,
 		// direction: 1.75 * Math.PI
 	});
@@ -31,11 +32,11 @@ function render() {
 	context.clearRect(0, 0, canvas.width, canvas.height);	// clear canvas from previous state
 
 	ballsState = ballsState.reduce(function(activeBalls, ball) {
-		drawBall(context, ball.x, ball.y, BALL_RADIUS);
+		drawBall(context, ball.x, ball.y, BALL_RADIUS, ball.color);
 
 		const nextBallState = nextState(ball);
 
-		if (nextBallState.speed > 0.2) {
+		if (canvas.height - nextBallState.y > BALL_RADIUS || nextBallState.speed > 0.2) {
 			activeBalls.push(nextBallState);
 		}
 
@@ -43,9 +44,11 @@ function render() {
 	}, []);
 }
 
-function drawBall(context, x, y, r) {
+function drawBall(context, x, y, r, color) {
+	context.fillStyle = color;
 	context.beginPath();
 	context.arc(x, y, r, 0, 2 * Math.PI);
+	context.closePath();
 	context.fill();
 }
 
@@ -82,12 +85,12 @@ window.addEventListener('click', function(oEvent) {
 
 function getSpeedX(ball) {
 	// Vx = V * Math.cos(angle)
-	return Math.abs(ball.speed * parseFloat(Math.cos(ball.direction).toFixed(2)));
+	return Math.abs(ball.speed * parseFloat(Math.cos(ball.direction).toFixed(5)));
 }
 
 function getSpeedY(ball) {
 	// Vy = V * Math.sin(angle)
-	return Math.abs(ball.speed * parseFloat(Math.sin(ball.direction).toFixed(2)));
+	return Math.abs(ball.speed * parseFloat(Math.sin(ball.direction).toFixed(5)));
 }
 
 function nextState(ball) {
@@ -164,6 +167,7 @@ function nextState(ball) {
 		x: nextX,
 		y: nextY,
 		speed: nextSpeed,
-		direction: nextDirection
+		direction: nextDirection,
+		color: ball.color
 	}
 }
